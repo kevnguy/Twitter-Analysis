da34b0d61a385b861961f76364e5f9ecdbb43665
kevnguy <56405177+kevnguy@users.noreply.github.com>
2022-06-06 14:31:50 -0700

    Edit pipeline to use cross-validation and word to vec

--- src/main/scala/edu/ucr/cs/cs167/App.scala
+++ src/main/scala/edu/ucr/cs/cs167/App.scala
@@ -2,10 +2,11 @@ package edu.ucr.cs.cs167
 
 import org.apache.spark.SparkConf
 import org.apache.spark.sql.{Row, SparkSession}
-import org.apache.spark.ml.Pipeline
-import org.apache.spark.ml.classification.LogisticRegression
-import org.apache.spark.ml.feature.{HashingTF, StringIndexer, Tokenizer}
-import org.apache.spark.mllib.evaluation.MulticlassMetrics
+import org.apache.spark.ml.{Pipeline, PipelineModel}
+import org.apache.spark.ml.classification.{LogisticRegression, LogisticRegressionModel}
+import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
+import org.apache.spark.ml.feature.{StringIndexer, Tokenizer, Word2Vec, Word2VecModel}
+import org.apache.spark.ml.tuning.{CrossValidator, ParamGridBuilder}
 import org.apache.spark.sql.functions.{array_intersect, col, concat_ws, lit, length}
 
 object App {
@@ -47,56 +48,68 @@ object App {
           finalDF.write.json("tweets_topic")
 
         case "Topic-Prediction" =>
+          println("Beginning topic prediction model building")
           val inputFile = args(1)
           var tweetsDF = sparkSession.read.json(inputFile)
 
+          // combine text columns
           tweetsDF = tweetsDF.withColumn("all_text",concat_ws(" ",col("text"),col("user_description")))
-          val Array(training, test) = tweetsDF.randomSplit(Array(0.7, 0.3))
-          tweetsDF.show()
 
-          // change inputCol to topic for actual data
           val indexer = new StringIndexer()
-            .setInputCol("hashtag")
+            .setInputCol("topic")
             .setOutputCol("label")
             .fit(tweetsDF)
-          val indexed = indexer.transform(tweetsDF)
 
-          // tokenize all text
-          val tokenizer = new Tokenizer()
+          val tokenizer = new Tokenizer() // tokenize all text
             .setInputCol("all_text")
             .setOutputCol("words")
 
-          // converts tokens to numeric features
-          val hashingTF = new HashingTF()
-            .setNumFeatures(20)
+          val word2vec = new Word2Vec() // word2vec instead of TF.IDF - can use seed for init
             .setInputCol(tokenizer.getOutputCol)
             .setOutputCol("features")
+            .setVectorSize(300)
 
-          // classifier - regParam is helps prevent overfitting?
-          val lr = new LogisticRegression()
+          val lr = new LogisticRegression() // Using logistic reg as classifier
             .setMaxIter(10)
-          //.setRegParam(0.001)
 
-          // pipeline
-          val pipeline = new Pipeline()
-            .setStages(Array(indexer,tokenizer,hashingTF,lr))
+          val pipeline = new Pipeline() // pipeline
+            .setStages(Array(indexer,tokenizer,word2vec,lr))
 
-          // build model with training split
-          val model = pipeline.fit(training)
+          val paramGrid = new ParamGridBuilder() // parameter grid for cross validation
+            .addGrid(word2vec.minCount, Array(0, 1))
+            .addGrid(lr.elasticNetParam, Array(0,0.01,0.1,0.3,0.8))
+            .addGrid(lr.regParam, Array(0,0.01,0.1,0.3,0.8))
+            .build()
 
-          // use model on test data
-          val results = model.transform(test)
-            .select("id", "text", "hashtag", "user_description", "label", "prediction")
+          val cv = new CrossValidator() // Running cross validator to produce best model given parameter grid
+            .setEstimator(pipeline)
+            .setEvaluator(new MulticlassClassificationEvaluator)
+            .setEstimatorParamMaps(paramGrid)
+            .setNumFolds(3)  // Use 3+ in practice
+            .setParallelism(3)  // Evaluate up to 3 parameter settings in parallel docs say ~10
+
+          val Array(training, test) = tweetsDF.randomSplit(Array(0.7, 0.3)) // splits data into training and test - can use seed
+          val model = cv.fit(training) // build model with training split
+          val minCount = model.bestModel.asInstanceOf[PipelineModel].stages(2).asInstanceOf[Word2VecModel].getMinCount
+          val elasticRegParam = model.bestModel.asInstanceOf[PipelineModel].stages(3).asInstanceOf[LogisticRegressionModel].getElasticNetParam
+          val RegParam = model.bestModel.asInstanceOf[PipelineModel].stages(3).asInstanceOf[LogisticRegressionModel].getRegParam
+
+          model.write.overwrite().save("models/logistic-regression-model") // save model
+
+          val results = model.transform(test) // use model on test data
+            .select("id", "text", "topic", "user_description", "label", "prediction")
 
           // get and print out metrics
-          val predictionAndLabels = results.select("prediction", "label")
-            .rdd.map{case Row(prediction: Double, label: Double) => (prediction, label)
-            }
-          val metrics = new MulticlassMetrics(predictionAndLabels)
+          val multiclassClassificationEvaluator = new MulticlassClassificationEvaluator()
+          val metrics = multiclassClassificationEvaluator.getMetrics(results)
           println("Summary Statistics")
           println(s"Accuracy = ${metrics.accuracy}")
           println(s"Weighted precision: ${metrics.weightedPrecision}")
           println(s"Weighted recall: ${metrics.weightedRecall}")
+          println("Best model parameters")
+          println(s"Best minCount: $minCount")
+          println(s"Best elasticRegParam: $elasticRegParam")
+          println(s"Best RegParam: $RegParam")
       }
     }
     finally {
